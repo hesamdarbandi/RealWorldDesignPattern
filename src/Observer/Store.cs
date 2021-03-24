9e1acdc3fa763a1f4d2015fe689bf889cc69e889
hesam <hesamdarbandi@hotmail.com>
2021-03-24 19:02:39 +0430

    finish store

--- src/Observer/Store.cs
+++ src/Observer/Store.cs
@@ -4,13 +4,55 @@ using System.Collections.Generic;
 namespace Observer
 {
     //this is subject or publisher class
-    public class Store
+    public class Store : IStore
     {
+        // 1 new apple product arrive
+        // 2 new google product arrive
+
+        private int State = 0;
         private readonly List<IObserver> _customers;
+        private const string STORE_LABLE = "store";
 
         public Store() =>  _customers = new List<IObserver>();
 
-       
+        public void AttacheCustomer(IObserver observer)
+        {
+            Console.WriteLine($"{STORE_LABLE} attache customer");
+            if (_customers.Contains(observer))
+                throw new Exception("customer already attached");
+
+            _customers.Add(observer);
+        }
+
+        public void DetachCustomer(IObserver observer)
+        {
+            Console.WriteLine($"{STORE_LABLE} detach customer");
+            _customers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine($"{STORE_LABLE} notify cutomers");
+            _customers.ForEach(en =>
+               en.Update(this)
+            );
+        }
+
+
+        public void RecieveNewAppleProducts()
+        {
+            this.State = 1;
+            Notify();
+        }
+
+        public void RecieveNewGoogleProducts()
+        {
+            this.State = 2;
+            Notify();
+
+        }
+
+
     }
 
     public interface IStore
