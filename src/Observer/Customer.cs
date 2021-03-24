9a5a79b4e0591606255b6232d95eab454f75b017
hesam <hesamdarbandi@hotmail.com>
2021-03-24 18:48:40 +0430

    add observer rela world project

---
+++ src/Observer/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Observer
+{
+    public abstract class Customer
+    {
+        
+    }
+
+    public class AppleLovers : Customer , IObserver
+    {
+
+    }
+
+    public class GoogleLovers : Customer , IObserver
+    {
+
+    }
+
+
+    public interface IObserver
+    {
+        void Update(Store store);
+    }
+
+    public interface 
+}
---
+++ src/Observer/Store.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    //this is subject or publisher class
+    public class Store
+    {
+        private readonly List<IObserver> _customers;
+
+        public Store() =>  _customers = new List<IObserver>();
+
+       
+    }
+
+    public interface IStore
+    {
+        void AttacheCustomer(IObserver observer);
+        void DetachCustomer(IObserver observer);
+        void Notify();
+        void RecieveNewAppleProducts();
+        void RecieveNewGoogleProducts();
+    }
+
+}
