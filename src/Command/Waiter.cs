59670707b904ca426d7103716f6fafb29109bc7f
hesam <hesamdarbandi@hotmail.com>
2021-03-26 17:24:17 +0430

    add invoker that using command

---
+++ src/Command/Waiter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Command
+{
+    // this is invoker class
+    public class Waiter
+    {
+        private ICommand _command;
+
+        public void SetCommand(ICommand command) => _command = command;
+
+        public void TaKeOrder() => _command.Execute();
+    }
+}
