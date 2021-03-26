a30e0a419148ee6bf8f54554e8024aaed0094b71
hesam <hesamdarbandi@hotmail.com>
2021-03-26 17:27:56 +0430

    add client that using commands

--- src/Command/Program.cs
+++ src/Command/Program.cs
@@ -6,7 +6,15 @@ namespace Command
     {
         static void Main(string[] args)
         {
-          
+
+            var waiter = new Waiter();
+            var newPitzaCommand = new CreatePizzaCommand("peperoni","small");
+            waiter.SetCommand(newPitzaCommand);
+            waiter.TaKeOrder();
+
+            var newSanCommand = new CreateSandwichCommand("hotdog", "big");
+            waiter.SetCommand(newSanCommand);
+            waiter.TaKeOrder();
         }
     }
 }
