b4b761cc8ebd41c145d8bd7334beea1d5f9bde1d
hesam <hesamdarbandi@hotmail.com>
2021-03-26 17:17:55 +0430

    add order commands

---
+++ src/Command/OrderCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Command
+{
+    // this is commands
+    public interface ICommand
+    {
+        public void Execute();
+    };
+
+    public class CreatePizzaCommand:  ICommand
+    {
+        private string _type;
+        private string _size;
+        private OrderMaker _orderMaker;
+
+        public CreatePizzaCommand(string type,string size)
+        {
+            _orderMaker = new OrderMaker();
+            _type = type;
+            _size = size;
+        }
+
+        public void Execute() => _orderMaker.CreatePitza(_type, _size);
+
+    }
+
+    public class CreateSandwichCommand : ICommand
+    {
+        private string _type;
+        private string _size;
+        private OrderMaker _orderMaker;
+
+        public CreateSandwichCommand(string type, string size)
+        {
+            _orderMaker = new OrderMaker();
+            _type = type;
+            _size = size;
+        }
+
+        public void Execute() => _orderMaker.CreateSandwich(_type, _size);
+
+    }
+
+}
---
+++ src/Command/OrderMaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Command
+{
+    //this is reciever class
+    public class OrderMaker
+    {
+        public void CreatePitza(string type, string size) => Console.WriteLine($"created {type} pitza with {size} size");
+
+        public void CreateSandwich(string type, string size) => Console.WriteLine($"created {type} pitza with {size} size");
+
+    }
+}
